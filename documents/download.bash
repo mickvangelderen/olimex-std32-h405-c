c55a599d9438d2094562aacb61ec5cae847cf495
Mick van Gelderen <mickvangelderen@gmail.com>
2018-02-06 14:52:06 +0100

    Improve document downloads

--- documents/download.bash
+++ documents/download.bash
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-wget http://openocd.org/doc-release/pdf/openocd.pdf
+# http://openocd.org/documentation/
+wget http://openocd.org/doc-release/pdf/openocd.pdf -O "openocd-user-guide.pdf"
 
-wget http://www.st.com/resource/en/datasheet/stm32f405rg.pdf
+# https://www.olimex.com/Products/ARM/ST/STM32-H405/
+wget https://www.olimex.com/Products/ARM/ST/STM32-H405/resources/STM32-H405_UM.pdf -O "olimex-stm32-h405-user-manual.pdf"
 
-wget https://www.olimex.com/Products/ARM/ST/STM32-H405/resources/STM32-H405_UM.pdf
+# http://www.st.com/en/microcontrollers/stm32f405rg.html
+wget http://www.st.com/resource/en/datasheet/stm32f405rg.pdf -O "stm32f405rg-datasheet.pdf"
+wget http://www.st.com/resource/en/reference_manual/dm00031020.pdf -O "stm32f405rg-reference-manual.pdf"
+wget http://www.st.com/resource/en/programming_manual/dm00046982.pdf -O "stm32f405rg-programming-manual.pdf"
+wget http://www.st.com/resource/en/errata_sheet/dm00037591.pdf -O "stm32f405rg-errata-sheet.pdf"
+
 
